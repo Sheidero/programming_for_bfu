388ed9e49d4aba097a83d811aa7a12518765394e
Sheidero <115668364+Sheidero@users.noreply.github.com>
2022-12-24 15:00:08 +0200

    Delete labs_for_bfu/lab_7 directory

--- labs_for_bfu/lab_7/CMakeFiles/3.25.0-rc1/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/arm64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/artemgavrikov/Documents/Builds/lab_7/CMakeFiles/3.25.0-rc1/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- labs_for_bfu/lab_7/funcs.hpp
+++
@@ -1,8 +0,0 @@
-
-namespace rd
-{
-    bool isSimple(int chislo);
-    bool reading(int n, int** matriz,int* prost);
-    void think(int n, int** matriz,int* prost);
-    void write(int n, int** matriz);
-}
