78bb2e6b5bb3ce7138bad5fc95beeaae0f2998ad
Sheidero <115668364+Sheidero@users.noreply.github.com>
2022-12-24 14:48:48 +0200

    Add files via upload

--- practise_for_bfu/practise_4/practise_4.cpp
+++ practise_for_bfu/practise_4/practise_4.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-struct chisla {
+struct Chisla {
     unsigned short chislo_1: 2;
     unsigned short chislo_2: 2;
     unsigned short chislo_3: 2;
@@ -8,21 +8,24 @@ struct chisla {
 };
 
 int main(){
-    chisla a;
+    Chisla a;
     int b;
     std::cout << "You can put here four numbers(0-2)" << "\n" << "Enter them" << std::endl;
     std::cout << "1st" << std::endl; std::cin >> b;
     a.chislo_1 = b;
     
-    std::cout << "2st" << std::endl; std::cin >> b;
+    std::cout << "2nd" << std::endl; std::cin >> b;
     a.chislo_2 = b;
     
-    std::cout << "3st" << std::endl; std::cin >> b;
+    std::cout << "3rd" << std::endl; std::cin >> b;
     a.chislo_3 = b;
     
-    std::cout << "4st" << std::endl; std::cin >> b;
-    
+    std::cout << "4th" << std::endl; std::cin >> b;
     a.chislo_4 = b;
-    std::cout << a.chislo_1 << std::endl;
+    
+    std::cout << "1st = " << a.chislo_1 << std::endl;
+    std::cout << "2nd = " << a.chislo_2 << std::endl;
+    std::cout << "3rd = " << a.chislo_3 << std::endl;
+    std::cout << "4th = " << a.chislo_4 << std::endl;
     return 0;
 }
