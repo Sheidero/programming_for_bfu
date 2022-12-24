7fd6423a05de3cbcc52f0d714d1373d7740f5e18
Sheidero <115668364+Sheidero@users.noreply.github.com>
2022-12-24 16:24:54 +0200

    Add files via upload

---
+++ labs_for_bfu/lab_7/funcs.hpp
@@ -0,0 +1,8 @@
+
+namespace rd
+{
+    bool isSimple(int chislo);
+    bool reading(int n, int** matriz,int* prost);
+    void think(int n, int** matriz,int* prost);
+    void write(int n, int** matriz);
+}
