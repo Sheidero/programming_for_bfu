163c6ced83db403615f23599b8ff0b74fac607a8
Sheidero <115668364+Sheidero@users.noreply.github.com>
2022-12-27 19:28:07 +0200

    Add files via upload

---
+++ labs_for_bfu/lab_11/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int a = 0;
+    int b = INT_MAX;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << b + 1 << std::endl;
+    return 0;
+}
